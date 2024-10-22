98bbbb5cc80f752079fbdb23355b57c17c68c525
math92n4 <math92n4@stud.kea.dk>
2024-10-22 15:14:20 +0200

    logs

--- src/main/java/com/example/jannygaardrabbitmq/controller/ProducerController.java
+++ src/main/java/com/example/jannygaardrabbitmq/controller/ProducerController.java
@@ -2,6 +2,8 @@ package com.example.jannygaardrabbitmq.controller;
 
 import com.example.jannygaardrabbitmq.service.RabbitMQSender;
 import com.example.jannygaardrabbitmq.entity.Car;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
@@ -9,18 +11,22 @@ import org.springframework.web.bind.annotation.*;
 @RequestMapping("/api")
 public class ProducerController {
 
+    private static final Logger logger = LoggerFactory.getLogger(ProducerController.class);
+
     @Autowired
     private RabbitMQSender rabbitMQSender;
 
     @PostMapping("/webhook/td/booking/create/{id}")
     public String td(@PathVariable int id) {
         rabbitMQSender.sendTd(id);
+        logger.info("RABBITMQ MESSAGE TO TD: ", id);
         return "Sent to TD " + id;
     }
 
     @PostMapping("/rabbitmq/sp")
     public String sp() {
         rabbitMQSender.sendSp("Message to SP");
+        logger.info("RABBITMQ MESSAGE TO SP");
         return "Sent to SP";
     }
 }
