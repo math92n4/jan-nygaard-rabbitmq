bac9429041b531327660ce66abfe431e64ab9aff
math92n4 <math92n4@stud.kea.dk>
2024-10-22 14:29:41 +0200

    changed message

--- src/main/java/com/example/jannygaardrabbitmq/controller/ProducerController.java
+++ src/main/java/com/example/jannygaardrabbitmq/controller/ProducerController.java
@@ -6,19 +6,19 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
-@RequestMapping("/rabbitmq")
+@RequestMapping("/api")
 public class ProducerController {
 
     @Autowired
     private RabbitMQSender rabbitMQSender;
 
-    @PostMapping("/td/{id}")
+    @PostMapping("/webhook/td/booking/create/{id}")
     public String td(@PathVariable int id) {
         rabbitMQSender.sendTd(id);
         return "Sent to TD " + id;
     }
 
-    @PostMapping("/sp")
+    @PostMapping("/rabbitmq/sp")
     public String sp() {
         rabbitMQSender.sendSp("Message to SP");
         return "Sent to SP";
